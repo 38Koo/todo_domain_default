9e657118bcab19c115896f83c9b531c0c834d230
38Koo <captain.marvel.mk@gmail.com>
2026-02-08 22:29:13 +0900

    環境変数とCORSの設定

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
+import dotenv from "dotenv";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { healthCheckController } from "./presentation/controllers/healthCheckController.js";
 import { ItemController } from "./presentation/controllers/itemController.js";
+import { cors } from "hono/cors";
+
+dotenv.config();
 
 const app = new Hono();
 
-app.route("/", healthCheckController);
-app.route("/items", ItemController);
+app.use(
+  "*",
+  cors({
+    origin: process.env.CLIENT_DOMAIN || "",
+    allowHeaders: ["X-Custom-Header"],
+    allowMethods: ["POST", "GET", "OPTIONS"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+  }),
+);
+
+app.route("/api", healthCheckController);
+app.route("/api/items", ItemController);
 
 serve(
   {
